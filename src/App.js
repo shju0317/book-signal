c7591c9069ae0c13c9872b5608eeee48c4a16320
김채린 <162540517+kkimchaerin@users.noreply.github.com>
2024-08-19 16:15:37 +0900

    회원가입 페이지 만들기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import GetReview from './pages/GetReview';
+import Join from './pages/Join';
 
 function App() {
   return (
     <div className="App">
       <div>프로젝트 시작</div>
-      <GetReview></GetReview>
+      <Join></Join>
     </div>
   );
 }
