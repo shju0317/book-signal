6be5ee46cfc9cfaab806c5e627cbe4f94e442ca9
김채린 <162540517+kkimchaerin@users.noreply.github.com>
2024-08-19 17:07:44 +0900

    getreview 페이지 front 복구

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Join from './pages/Join';
 import Chatbot from './components/Chatbot';
+import GetReview from './pages/GetReview';
 
 function App() {
   return (
     <div className="App">
       <div>프로젝트 시작</div>
       <Join></Join>
+      <GetReview></GetReview>
     </div>
   );
 }
