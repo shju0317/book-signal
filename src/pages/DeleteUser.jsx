64ce3a1cbe32792a27e4d896280729f128fec535
김채린 <162540517+kkimchaerin@users.noreply.github.com>
2024-08-21 11:22:59 +0900

    회원탈퇴 프론트 완성

--- src/pages/DeleteUser.jsx
+++ src/pages/DeleteUser.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react';
+import '../css/deleteuser.css'
 
 const DeleteUser = () => {
+
+  const [memId, setMemId] = useState('');
+  const [memPw, setMemPw] = useState('');
+
   return (
-    <div>DeleteUser</div>
+    <div className='deleteuser-container'>
+      <div className='title-container'>
+        <h1 className='title-book'>북</h1>
+        <h1 className='title-signal'>시그널</h1>
+      </div>
+      <div className="login-container">
+        <h4 className="login-title">회원탈퇴</h4>
+        <hr />
+        <br />
+        <form className="deleteuser-form">
+          <div className="input-group">
+            <input
+              type="text"
+              className="input-field"
+              id="memId"
+              name="memId"
+              placeholder="아이디 입력"
+              value={memId}
+              onChange={() => setMemId()}
+            />
+          </div>
+          <div className="input-group">
+            <input
+              type="password"
+              className="input-field"
+              id="memPw"
+              name="memPw"
+              placeholder="비밀번호 입력"
+              value={memPw}
+              onChange={() => setMemPw()}
+            />
+          </div>
+          <button type="submit" className="login-button">로그인</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
