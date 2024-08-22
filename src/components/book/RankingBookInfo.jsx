794566e17fd1e804f0ba908441e56ab8aa8641fc
신현주 <31871923+shju0317@users.noreply.github.com>
2024-08-22 15:38:42 +0900

    RankingBookList 페이지 css 수정

--- src/components/book/RankingBookInfo.jsx
+++ src/components/book/RankingBookInfo.jsx
@@ -7,9 +7,9 @@ const RankingBookInfo = () => {
       <div className='book-cover-wrapper w-[100px] h-[150px]'>
         <img src='book_cover_sample.jpg' alt="이미지" className='w-full h-full object-cover rounded-xl'/>
       </div>
-      <div className='book-info-contents flex flex-col gap-1 text-md px-5'>
+      <div className='book-info-contents flex flex-col text-md px-5 my-auto'>
         <p className='text-3xl font-bold italic text-primary'>1</p>
-        <p className='text-xl font-semibold mb-1'>나의 눈부신 친구</p>
+        <p className='text-xl font-semibold mb-5'>나의 눈부신 친구</p>
         <div className='flex gap-3'>
           <p>홍길동<span>저자</span></p>
           <p className='before:content-["|"] before:mr-3'>홍길순<span>옮김</span></p>
