66b546ae0887dfd51bd00d23cb75a2a820a2f8d6
김채린 <162540517+kkimchaerin@users.noreply.github.com>
2024-08-29 09:30:09 +0900

    의존성 해결

--- src/pages/BookDetail.jsx
+++ src/pages/BookDetail.jsx
@@ -39,155 +39,155 @@ const BookDetail = () => {
         console.error('로그인 상태 확인 중 오류 발생:', error);
         setIsLoggedIn(false);
       });
-  }, [book.book_idx]);
+  }, [book, navigate]);
 
 
   //////////////////////////////////////////////////////////////////////////////////////////
   useEffect(() => {
     // 관련 도서 가져오기
-    axios.get('http://localhost:3001/sameBook',  {
+    axios.get('http://localhost:3001/sameBook', {
       params: { genre: book.book_genre, idx: book.book_idx }
     })
-    .then(response => {
-      const { sameBooks } = response.data;
-      setSameBooks(sameBooks);
-      console.log(sameBooks);
-    })
-    .catch(error => {
-      console.error('오류:', error.response ? error.response.data : error.message);
-    });
-}, []);
-//////////////////////////////////////////////////////////////////////////////////////////
+      .then(response => {
+        const { sameBooks } = response.data;
+        setSameBooks(sameBooks);
+        console.log(sameBooks);
+      })
+      .catch(error => {
+        console.error('오류:', error.response ? error.response.data : error.message);
+      });
+  }, [book.book_genre, book.book_idx]);
+  //////////////////////////////////////////////////////////////////////////////////////////
 
 
 
-// 사용자가 이미 찜한 도서인지 확인
-const checkWishlistStatus = async (memId, bookIdx) => {
-  try {
-    const response = await axios.post('http://localhost:3001/wishlist/check', { mem_id: memId, book_idx: bookIdx });
-    setIsWishlisted(response.data.isWishlisted);
-  } catch (error) {
-    console.error('찜한 도서 상태 확인 실패:', error);
-  }
-};
-
-const handleWishList = async () => {
-  if (!isLoggedIn) {
-    alert('로그인이 필요합니다.');
-    navigate('/login');
-    return;
-  }
-
-  try {
-    if (isWishlisted) {
-      await axios.post('http://localhost:3001/wishlist/remove', { mem_id: memId, book_idx: book.book_idx });
-      alert('찜한 도서에서 제거되었습니다.');
-    } else {
-      await axios.post('http://localhost:3001/wishlist', { mem_id: memId, book_idx: book.book_idx });
-      alert('찜한 도서에 추가되었습니다.');
+  // 사용자가 이미 찜한 도서인지 확인
+  const checkWishlistStatus = async (memId, bookIdx) => {
+    try {
+      const response = await axios.post('http://localhost:3001/wishlist/check', { mem_id: memId, book_idx: bookIdx });
+      setIsWishlisted(response.data.isWishlisted);
+    } catch (error) {
+      console.error('찜한 도서 상태 확인 실패:', error);
     }
-    setIsWishlisted(!isWishlisted);
-  } catch (error) {
-    console.error('찜한 도서 처리 실패:', error);
-  }
-};
+  };
 
-const handleReadBook = async () => {
-  if (!isLoggedIn) {
-    alert('로그인이 필요합니다.');
-    navigate('/login');
-    return;
-  }
+  const handleWishList = async () => {
+    if (!isLoggedIn) {
+      alert('로그인이 필요합니다.');
+      navigate('/login');
+      return;
+    }
 
-  try {
-    // 독서 기록을 데이터베이스에 추가
-    await axios.post('http://localhost:3001/addReadingRecord', { mem_id: memId, book_name: book.book_name });
+    try {
+      if (isWishlisted) {
+        await axios.post('http://localhost:3001/wishlist/remove', { mem_id: memId, book_idx: book.book_idx });
+        alert('찜한 도서에서 제거되었습니다.');
+      } else {
+        await axios.post('http://localhost:3001/wishlist', { mem_id: memId, book_idx: book.book_idx });
+        alert('찜한 도서에 추가되었습니다.');
+      }
+      setIsWishlisted(!isWishlisted);
+    } catch (error) {
+      console.error('찜한 도서 처리 실패:', error);
+    }
+  };
 
-    // 책의 경로를 가져와서 리더 페이지로 이동
-    const bookNameWithoutSpaces = book.book_name.replace(/\s+/g, '');
+  const handleReadBook = async () => {
+    if (!isLoggedIn) {
+      alert('로그인이 필요합니다.');
+      navigate('/login');
+      return;
+    }
 
-    const response = await axios.post('http://localhost:3001/getBookPath', {
-      book_name: encodeURIComponent(bookNameWithoutSpaces)
-    });
+    try {
+      // 독서 기록을 데이터베이스에 추가
+      await axios.post('http://localhost:3001/addReadingRecord', { mem_id: memId, book_name: book.book_name });
 
-    const bookPath = response.data.book_path;
-    navigate('/reader', { state: { bookPath } });
-  } catch (error) {
-    console.error('책 읽기 처리 중 에러:', error);
-    alert('책을 읽는 중에 문제가 발생했습니다.');
-  }
+      // 책의 경로를 가져와서 리더 페이지로 이동
+      const bookNameWithoutSpaces = book.book_name.replace(/\s+/g, '');
+
+      const response = await axios.post('http://localhost:3001/getBookPath', {
+        book_name: encodeURIComponent(bookNameWithoutSpaces)
+      });
+
+      const bookPath = response.data.book_path;
+      navigate('/reader', { state: { bookPath } });
+    } catch (error) {
+      console.error('책 읽기 처리 중 에러:', error);
+      alert('책을 읽는 중에 문제가 발생했습니다.');
+    }
 
 
-};
+  };
 
 
-return (
-  <div className='book-info-wrapper flex flex-col gap-10 max-w-screen-xl m-auto'>
-    <section className='flex gap-6 w-30 h-96 justify-between'>
-      <h2 className='sr-only'>도서 정보</h2>
-      <div className='book-cover-wrapper w-[250px] aspect-w-2 aspect-h-3'>
-        <img src={book?.book_cover} alt={book?.book_name || "도서 이미지"} className='w-full h-full object-cover rounded-xl' />
-      </div>
-      <div className='border rounded-xl grow flex flex-col justify-between'>
-        <div className='book-info-contents flex flex-col gap-2 text-md p-6'>
-          <p className='text-[14px] text-gray-500 mt-2'>{book.book_genre}</p>
-          <p className='text-3xl font-semibold mb-4'>{book.book_name}</p>
-          <div className='flex gap-3'>
-            <p className='text-[16px]'>{book?.book_writer}<span>저자</span></p>
-          </div>
-          <p className='mb-4 text-[16px]'>{book.book_published_at}<span>출간</span></p>
-          <div className='flex ml-[-6px] mb-2'>
-            <span className='mr-2 text-[16px]'>이 책을 읽은 독자</span>
-            <p className='text-[16px] text-black'>{book.book_views}</p>
-          </div>
-          <div>
-            <p className='flex items-center text-[16px] justify-start mb-2'>
-              <span className='sr-only'>평점</span>
-              <TiStarFullOutline className='mt-[2px]' />
-              <strong className='ml-1 text-[16px]'>{book.book_avg}</strong>
-            </p>
-          </div>
+  return (
+    <div className='book-info-wrapper flex flex-col gap-10 max-w-screen-xl m-auto'>
+      <section className='flex gap-6 w-30 h-96 justify-between'>
+        <h2 className='sr-only'>도서 정보</h2>
+        <div className='book-cover-wrapper w-[250px] aspect-w-2 aspect-h-3'>
+          <img src={book?.book_cover} alt={book?.book_name || "도서 이미지"} className='w-full h-full object-cover rounded-xl' />
         </div>
-        <div className='book-info-button flex'>
-          <button onClick={handleReadBook} className='border-t border-r rounded-bl-xl flex-1 text-primary'>바로 읽기</button>
-          <button onClick={handleWishList} className={`border-t flex items-center justify-center rounded-br-xl flex-1 ${isWishlisted ? 'bg-primary text-white' : 'text-primary'}`}>
-            <IoMdHeartEmpty size={22} className='mr-2' />내 서재에 찜하기
-          </button>
-        </div>
-      </div>
-    </section>
-    <section>
-      <h2 className='text-2xl text-black'>책 소개</h2>
-      <div className='bg-background p-6 mt-3 rounded-xl'>
-        <h3 className='text-xl mb-3 '>도서 정보</h3>
-        <p className='font-normal'>{book.book_explanation}</p>
-      </div>
-    </section>
-    <section>
-      <h2 className='text-2xl mb-6 text-black'>관련 도서</h2>
-      <div className='flex gap-4'>
-        {sameBooks.map((sameBook) => (
-          <div className="same-book-card " >
-            <div key={sameBook.book_idx} className='book-cover-wrapper w-[200px] aspect-w-2 aspect-h-3'>
-              <img src={sameBook.book_cover} alt={sameBook.book_name} className='w-[180px] h-[260px] rounded-lg object-cover'/>
+        <div className='border rounded-xl grow flex flex-col justify-between'>
+          <div className='book-info-contents flex flex-col gap-2 text-md p-6'>
+            <p className='text-[14px] text-gray-500 mt-2'>{book.book_genre}</p>
+            <p className='text-3xl font-semibold mb-4'>{book.book_name}</p>
+            <div className='flex gap-3'>
+              <p className='text-[16px]'>{book?.book_writer}<span>저자</span></p>
             </div>
-            <div className="main-book-info w-[180px]">
-              <p className="main-book-title">{sameBook.book_name}</p>
-              <p className="main-book-author">{sameBook.book_writer}</p>
+            <p className='mb-4 text-[16px]'>{book.book_published_at}<span>출간</span></p>
+            <div className='flex ml-[-6px] mb-2'>
+              <span className='mr-2 text-[16px]'>이 책을 읽은 독자</span>
+              <p className='text-[16px] text-black'>{book.book_views}</p>
+            </div>
+            <div>
+              <p className='flex items-center text-[16px] justify-start mb-2'>
+                <span className='sr-only'>평점</span>
+                <TiStarFullOutline className='mt-[2px]' />
+                <strong className='ml-1 text-[16px]'>{book.book_avg}</strong>
+              </p>
             </div>
           </div>
-        ))}
+          <div className='book-info-button flex'>
+            <button onClick={handleReadBook} className='border-t border-r rounded-bl-xl flex-1 text-primary'>바로 읽기</button>
+            <button onClick={handleWishList} className={`border-t flex items-center justify-center rounded-br-xl flex-1 ${isWishlisted ? 'bg-primary text-white' : 'text-primary'}`}>
+              <IoMdHeartEmpty size={22} className='mr-2' />내 서재에 찜하기
+            </button>
+          </div>
+        </div>
+      </section>
+      <section>
+        <h2 className='text-2xl text-black'>책 소개</h2>
+        <div className='bg-background p-6 mt-3 rounded-xl'>
+          <h3 className='text-xl mb-3 '>도서 정보</h3>
+          <p className='font-normal'>{book.book_explanation}</p>
+        </div>
+      </section>
+      <section>
+        <h2 className='text-2xl mb-6 text-black'>관련 도서</h2>
+        <div className='flex gap-4'>
+          {sameBooks.map((sameBook) => (
+            <div className="same-book-card " key={sameBook.book_idx}>
+              <div className='book-cover-wrapper w-[200px] aspect-w-2 aspect-h-3'>
+                <img src={sameBook.book_cover} alt={sameBook.book_name} className='w-[180px] h-[260px] rounded-lg object-cover' />
+              </div>
+              <div className="main-book-info w-[180px]">
+                <p className="main-book-title">{sameBook.book_name}</p>
+                <p className="main-book-author">{sameBook.book_writer}</p>
+              </div>
+            </div>
+          ))}
 
-      </div>
-    </section>
-    <div className='h-40 text-right'>
+        </div>
+      </section>
+      <div className='h-40 text-right'>
         <hr />
         <div className='h-5'></div>
         <span className='text-gray-400'>b:ook</span>
         <div className='h-10'></div>
       </div>
-  </div>
-);
+    </div>
+  );
 };
 
 export default BookDetail;
